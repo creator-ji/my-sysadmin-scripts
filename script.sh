5a9f4277906dd0f9a51f87648a67ceb7d497d963
zhibeka <zhibeka.bft@gmail.com>
2026-09-11 14:21:43 +0300

    fix: обработка отсутствующего файла лога

--- script.sh
+++ script.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 LOGFILE="/var/log/syslog"
+if [ ! -f "$LOGFILE" ]; then
+echo "Ошибка: файл $LOGFILE не найден" >&2
+exit 1
+fi
 grep -aiE "error|fail" "$LOGFILE" > report.txt
+echo "Готово: report.txt содержит $(wc -l < report.txt) строк"
